f3512e086cb09c5db8ae09ba90cb18e3844591cd
Ivan Tarasov <ivan.tarasov.ca@gmail.com>
2023-08-30 16:16:20 -0400

    Fix: Amazon S3 accept only http connection

--- src/AmazonS3.php
+++ src/AmazonS3.php
@@ -6,10 +6,16 @@ use TripBuilder\Debug\dBug;
 
 class AmazonS3
 {
+    const PROTOCOL_HTTP  = 'http://',
+          PROTOCOL_HTTPS = 'https://',
+          PROTOCOL_VOID  = '//';
+
+
     public static function getUrl(?string $url = null): string
     {
         return sprintf(
-            '//%s.s3-website.%s.amazonaws.com%s',
+            '%s%s.s3-website.%s.amazonaws.com%s',
+            self::PROTOCOL_HTTP,
             $_ENV['AWS_BUCKET'],
             $_ENV['AWS_REGION'],
             !empty($url)
