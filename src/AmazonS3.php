5bcc05900e4f254b69e1b888466a8f2c93d21dd7
Ivan Tarasov <ivan.tarasov.ca@gmail.com>
2023-08-30 17:38:20 -0400

    Fix: Added AWS Cloudfront support

--- src/AmazonS3.php
+++ src/AmazonS3.php
@@ -6,18 +6,15 @@ use TripBuilder\Debug\dBug;
 
 class AmazonS3
 {
-    const PROTOCOL_HTTP  = 'http://',
-          PROTOCOL_HTTPS = 'https://',
-          PROTOCOL_VOID  = '//';
-
-
+    /**
+     * @param string|null $url
+     * @return string
+     */
     public static function getUrl(?string $url = null): string
     {
         return sprintf(
-            '%s%s.s3-website.%s.amazonaws.com%s',
-            self::PROTOCOL_HTTP,
-            $_ENV['AWS_BUCKET'],
-            $_ENV['AWS_REGION'],
+            '//%s%s',
+            $_ENV['AWS_CLOUDFRONT'],
             !empty($url)
                 ? '/' . $url
                 : null
