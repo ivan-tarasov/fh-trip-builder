bcc5f686b48e3289544872c22576b14a7d0db3a8
Ivan Tarasov <ivan.tarasov.ca@gmail.com>
2023-09-06 20:53:34 -0400

    Change API from dev to prod

--- config/common/api.php
+++ config/common/api.php
@@ -13,12 +13,12 @@ return [
 
     'fake' => [
         // DEV credentials
-        'url'   => 'http://localhost:8888/api',
-        'token' => 'SomeAPItoken_$ecretWORD---orHASH',
+        // 'url'   => 'http://localhost:8888/api',
+        // 'token' => 'SomeAPItoken_$ecretWORD---orHASH',
 
         // PROD credentials
-        // 'url'   => 'https://trip-builder.tarasov.ca/api',
-        // 'token' => 'SomeAPItoken_$ecretWORD---orHASH',
+        'url'   => 'https://trip-builder.tarasov.ca/api',
+        'token' => 'SomeAPItoken_$ecretWORD---orHASH',
     ],
 
 ];
