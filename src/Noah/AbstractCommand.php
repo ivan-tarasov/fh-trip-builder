f1d9bcd6f145e74541f1e1a2984849fe7e219ab2
Ivan Tarasov <ivan.tarasov.ca@gmail.com>
2025-08-25 19:05:16 -0400

    Refactored Noah AbstractCommand class

--- src/Noah/AbstractCommand.php
+++ src/Noah/AbstractCommand.php
@@ -3,9 +3,10 @@
 namespace TripBuilder\Noah;
 
 use Dotenv\Dotenv;
+use Exception;
+use MysqliDb;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -13,7 +14,7 @@ use TripBuilder\Helper;
 
 abstract class AbstractCommand extends Command
 {
-    const NOAH_VERSION = 'v0.3.3';
+    private const NOAH_VERSION = 'v0.3.4';
 
     /**
      * Color presets
@@ -24,44 +25,41 @@ abstract class AbstractCommand extends Command
      *         black, red,        green,        yellow,        blue,        magenta,        cyan,        white,
      *         gray,  bright-red, bright-green, bright-yellow, bright-blue, bright-magenta, bright-cyan, bright-white
      */
-    const STYLE_FORMATS = [
-        'default'   => ['default', 'default'],
-        'primary'   => ['white', 'blue'],
-        'success'   => ['bright-white', 'green'],
-        'danger'    => ['white', 'red'],
-        'warning'   => ['black', 'yellow'],
-        'info'      => ['bright-white', 'cyan'],
-        'light'     => ['black', 'white'],
-        'dark'      => ['white', 'black'],
+    private const STYLE_FORMATS = [
+        'default' => ['default', 'default'],
+        'primary' => ['white', 'blue'],
+        'success' => ['bright-white', 'green'],
+        'danger' => ['white', 'red'],
+        'warning' => ['black', 'yellow'],
+        'info' => ['bright-white', 'cyan'],
+        'light' => ['black', 'white'],
+        'dark' => ['white', 'black'],
     ];
 
-    const INDICATOR_SPINNER = [
+    private const INDICATOR_SPINNER = [
         ['⠏', '⠛', '⠹', '⢸', '⣰', '⣤', '⣆', '⡇'],
         ['▁','▂','▃','▄','▅','▆','▇','█','▇','▆','▅','▄','▃','▁'],
         ['◜','◝','◞','◟'],
         ['◐','◓','◑','◒'],
     ];
 
-    const FORMAT_OUTPUT_LINE_LENGTH   = 60,
-          FORMAT_OUTPUT_DEFAULT_COLOR = 'default';
+    private const FORMAT_OUTPUT_LINE_LENGTH = 60;
+    private const FORMAT_OUTPUT_DEFAULT_COLOR = 'default';
 
-    const CONFIG_DIR_TABLES  = 'noah/db/tables',
-          CONFIG_DIR_SEEDERS = 'noah/db/seeders';
+    protected const CONFIG_DIR_TABLES  = 'noah/db/tables';
+    protected const CONFIG_DIR_SEEDERS = 'noah/db/seeders';
 
-    protected $input;
-
-    protected $output;
-
-    protected $io;
-
-    protected $db;
+    protected InputInterface $input;
+    protected OutputInterface $output;
+    protected SymfonyStyle $io;
+    protected MysqliDb $db;
 
     protected function initialize(InputInterface $input, OutputInterface $output): void
     {
         parent::initialize($input, $output);
 
-        // Setting-up input and output global variables
-        $this->input  = $input;
+        // Setting up input and output global variables
+        $this->input = $input;
         $this->output = $output;
 
         // Define IO instance
@@ -80,9 +78,6 @@ abstract class AbstractCommand extends Command
         $this->databaseConnect();
     }
 
-    /**
-     * @return void
-     */
     private function headerMessage(): void
     {
         $this->io->writeln('
@@ -92,58 +87,55 @@ abstract class AbstractCommand extends Command
  ██║╚██╗██║██║   ██║██╔══██║██╔══██║
  ██║ ╚████║╚██████╔╝██║  ██║██║  ██║
  ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ' . self::NOAH_VERSION);
-        $this->io->writeln(sprintf(
-            ' <primary>%s</primary><warning>%s</warning>',
-            '#StandWith',
-            'Ukraine'
-        ));
+        $this->io->writeln(
+            sprintf(
+                ' <primary>%s</primary><warning>%s</warning>',
+                '#StandWith',
+                'Ukraine',
+            ),
+        );
 
         $this->io->newLine();
     }
 
-    /**
-     * @return void
-     */
     private function commandInformation(): void
     {
-        $this->io->writeln(sprintf(
-            ' <info>  %s </info>',
-            $this->getDescription()
-        ));
+        $this->io->writeln(
+            sprintf(
+                ' <info>  %s </info>',
+                $this->getDescription(),
+            ),
+        );
 
         $this->io->newLine();
     }
 
-    /**
-     * @return void
-     */
     private function databaseConnect(): void
     {
         $dotenv = Dotenv::createImmutable(Helper::getRootDir());
         $dotenv->load();
 
-        $this->db = new \MysqliDb(
+        $this->db = new MysqliDb(
             // TODO: for local tests use 127.0.0.1
-            // '127.0.0.1',
             $_ENV['DB_HOST'],
             $_ENV['DB_USERNAME'],
             $_ENV['DB_PASSWORD'],
             $_ENV['DB_DATABASE'],
-            $_ENV['DB_PORT']
+            $_ENV['DB_PORT'],
         );
 
         // Enable tracer
-        $this->db->setTrace (true);
+        $this->db->setTrace(true);
     }
 
     /**
-     * @return array
+     * @throws Exception
      */
     protected function getAllDatabaseTables(): array
     {
         $request = sprintf(
             "SELECT table_name FROM information_schema.tables WHERE table_schema = '%s' AND table_type = 'BASE TABLE'",
-            $_ENV['DB_DATABASE']
+            $_ENV['DB_DATABASE'],
         );
 
         $response = $this->db->rawQuery($request);
@@ -159,9 +151,6 @@ abstract class AbstractCommand extends Command
         return $tables;
     }
 
-    /**
-     * @return void
-     */
     private function buildFormats(): void
     {
         foreach (self::STYLE_FORMATS as $name => $colors) {
@@ -170,35 +159,28 @@ abstract class AbstractCommand extends Command
         }
     }
 
-    /**
-     * @param string $text
-     * @param string $status
-     * @param string $color
-     * @param bool   $newline
-     * @return void
-     */
     protected function formatOutput(
         string $text,
         string $status,
         string $color = self::FORMAT_OUTPUT_DEFAULT_COLOR,
         bool $newline = false
-    ): void
-    {
+    ): void {
         $times = strlen($status);
 
         $dots = str_repeat('.', self::FORMAT_OUTPUT_LINE_LENGTH - strlen($text) - $times);
 
-        $this->output->writeln(sprintf(
-            ' %1$s %2$s <%4$s> %3$s </%4$s>',
-            $text,
-            $dots,
-            strtoupper($status),
-            $color
-        ));
+        $this->output->writeln(
+            sprintf(
+                ' %1$s %2$s <%4$s> %3$s </%4$s>',
+                $text,
+                $dots,
+                strtoupper($status),
+                $color,
+            ),
+        );
 
         if ($newline) {
             $this->io->newLine();
         }
     }
-
 }
